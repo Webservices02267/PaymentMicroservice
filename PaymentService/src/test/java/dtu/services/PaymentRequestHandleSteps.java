d27618ba5cd84526b6b208ad1d41ec1ef09d0a88
Uniel <adam@saunte.dk>
2022-01-14 16:43:34 +0100

    Fixed failed tests

--- PaymentService/src/test/java/dtu/services/PaymentRequestHandleSteps.java
+++ PaymentService/src/test/java/dtu/services/PaymentRequestHandleSteps.java
@@ -161,7 +161,7 @@ public class PaymentRequestHandleSteps {
         final boolean valid_token = service.doTokenVerificationResponseEvent(token, customerId);
         //TODO: Make sure that the token to payment mapping is saved in the payment service between calls
         //Correlation between payment -> token
-        tokenEvent = new Event(event_name, new Object[] {customerId, payment.getToken(), null });
+        tokenEvent = new Event(event_name, new Object[] {customerId, payment.getToken(), valid_token });
         verify(messageQueue).publish(tokenEvent);
     }
 
